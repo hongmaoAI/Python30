5ccd4889fd9c4210ae9b3eabfc67db37831c471d
Admin <1500839960@qq.com>
2025-08-24 00:30:39 +0800

    first commit python

---
+++ dataType.py
@@ -0,0 +1,31 @@
+# python 中的数据类型
+# 整数 1 2 3
+print(1)
+# 浮点数 1.2 -3.2
+print('float data', 1.2)
+"""
+定义一个复数 z=3+2j
+输出一个复数 result=3+2j 
+复数的实部 result.real=3
+复数的虚部 result.imag=2
+"""
+z = 3 + 2j
+print('result=', z)
+print('result.real', z.real)
+print('result.imag', z.imag)
+"""
+字符串
+'hello world'
+"python NiuBi"
+"""
+print('result 单引号', 'hello world')
+print('result 双引号', "python NiuBi")
+"""
+布尔值 True or Flase
+天是亮的吗？如果天是亮的，那么值是True
+天是亮的吗？如果天是不亮的，那么值是flase
+布尔值涉及到运算符时，值会被隐式转为数值
+"""
+print('天是亮的吗？如果天不是亮的，那么值是', False)
+print('天是亮的吗？如果天是亮的，那么值是', True)
+print(True + 2)
---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print('hello world')
+# 加法 result=5
+print('add result',2+3)
+# 减法
+print('sub result',3-2)
+# 乘法
+print('mul result',3*2)
+# 除法
+print('div result',3/2)
+# 运算符 % result取余数
+print('rem result',5%2)
+# 运算符 // result 删除余数
+print('remove rem result',5//2)
