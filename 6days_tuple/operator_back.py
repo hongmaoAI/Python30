45c1af77ee8cb80e6ffafa1c42d35bdab87fe9a4
Admin <1500839960@qq.com>
2025-10-04 19:48:57 +0800

    Submit 6days_tuple/operator_back

--- 6days_tuple/operator_back.py
+++ 6days_tuple/operator_back.py
@@ -20,3 +20,34 @@ family_members_list = family_brother_list + family_sister_list
 print(family_members_list)
 family_members = tuple(family_members_list)
 print(family_members)
+
+# 2级
+# 从 family_members 中获取兄弟姐妹和父母
+family_members_brother = family_members[0:6]
+family_members_sister = family_members[7:10]
+# 创建 fruits、vegetables 和 animal products 元组。连接三个元组并将其分配给名为 food_stuff_tp 的变量。
+fruits = ('apple', 'orange', 'banana')
+vegetables = ('potato', 'cabbage')
+animal_products = ('Cat_Food', 'Dog_Food')
+food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+# 将 food_stuff_tp 元组更改为 food_stuff_lt 列表
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
+# 从 food_stuff_tp 元组或 food_stuff_lt 列表中切出中间项或项。
+print(len(food_stuff_tp))
+middle_food_stuff_lt = food_stuff_lt[int(len(food_stuff_tp) / 2)]
+print(middle_food_stuff_lt)  # potato
+# 从 food_staff_lt 列表中切出前三项和最后三项
+top_three = food_stuff_lt[:3]
+print(top_three)  # ['apple', 'orange', 'banana']
+last_three = food_stuff_lt[-3:]
+print(last_three)  # ['cabbage', 'Cat_Food', 'Dog_Food']
+# 完全删除 food_staff_tp 元组
+del food_stuff_tp
+# 检查元组中是否存在项：
+# 检查 'Estonia' 是否在 nordic_country 元组中
+# 检查 'Iceland' 是否在 nordic_country 元组中
+nordic_countries = ('Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)  # False
+print('lceland' in nordic_countries)  # False
